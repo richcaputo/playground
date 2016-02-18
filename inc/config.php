23d7495c8e3b45ae2f90564b3e4351a27adb077a
Rich Caputo <rich@caputodesignz.com>
2016-02-17 20:07:56 -0500

    Start
    Full php playground project

---
+++ inc/config.php
@@ -0,0 +1,12 @@
+<?php
+
+    // these two constants are used to create root-relative web addresses
+    // and absolute server paths throughout all the code
+
+	define("BASE_URL","/playground/");
+	define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/playground/");
+	
+	define("DB_HOST","localhost");
+	define("DB_NAME","pninja_playground");
+	define("DB_USER","pninja");
+	define("DB_PASS","sEtjhKyD7ua9");
